12bc874b850be62a324a09afb25e4c2b1f964e2d
suay <suaytian@163.com>
2021-04-01 17:00:02 +0800

    eureka

--- springcloud-tian-consumer/src/main/java/com/tian/springcloud/SpringCloudConsumerApplication.java
+++ springcloud-tian-consumer/src/main/java/com/tian/springcloud/SpringCloudConsumerApplication.java
@@ -2,8 +2,10 @@ package com.tian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConsumerApplication.class, args);
