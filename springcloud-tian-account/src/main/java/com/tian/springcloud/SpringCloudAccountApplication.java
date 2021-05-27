43f271a7ec88b8806a648fc1f9f21f4fad4fcfe4
suay <suaytian@163.com>
2021-05-27 13:52:24 +0800

    Multi environment configuration

---
+++ springcloud-tian-account/src/main/java/com/tian/springcloud/SpringCloudAccountApplication.java
@@ -0,0 +1,13 @@
+package com.tian.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudAccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAccountApplication.class, args);
+    }
+}
---
+++ springcloud-tian-user/src/main/java/com/tian/springcloud/SpringCloudUserApplication.java
@@ -0,0 +1,25 @@
+package com.tian.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker //开启服务熔断 不用@enableHystrix 否则会报
+public class SpringCloudUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUserApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
+    }
+}
